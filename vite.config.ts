71944aa6947a9f6d412c0083d28340ffcdde89ee
farazzy4u <farazzy4u@gmail.com>
2025-02-15 10:00:33 -0600

    updated vitejs config with base

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/wake-up-for-suhoor/',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
