4ceaba3e22b8e23359366d7e6841c17066ec6762
farazzy4u <farazzy4u@gmail.com>
2025-02-15 16:45:40 -0600

    chore: add GitHub Actions deployment workflow

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/wake-up-for-suhoor/',
+  base: '/wake-up-for-suhoor-v1/',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
