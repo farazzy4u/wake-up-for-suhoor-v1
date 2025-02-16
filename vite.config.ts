cd1b6b44b08bd9cc23f6761e0408727eeb222773
farazzy4u <farazzy4u@gmail.com>
2025-02-15 19:47:43 -0600

    fix: update asset paths and build configuration

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig(({ command }) => ({
   },
   build: {
     outDir: 'dist',
-    assetsDir: 'assets',
+    assetsDir: '',
     sourcemap: true,
     copyPublicDir: true
   },
