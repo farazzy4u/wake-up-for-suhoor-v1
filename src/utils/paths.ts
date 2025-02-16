15cb31e87063c0b84d2ec72d27f2c3b1a8a09042
farazzy4u <farazzy4u@gmail.com>
2025-02-16 17:05:03 -0600

    updated file

--- src/utils/paths.ts
+++ src/utils/paths.ts
@@ -1,4 +1,8 @@
 const isProd = import.meta.env.PROD;
 const BASE_URL = isProd ? '/wake-up-for-suhoor-v1' : '';
 
-export const getAssetPath = (path: string) => `${BASE_URL}${path}`; 
+export const getAssetPath = (path: string) => {
+    // Remove leading slash if it exists
+    const cleanPath = path.startsWith('/') ? path.slice(1) : path;
+    return `${BASE_URL}/${cleanPath}`;
+}; 
