31c892857e1507a7161044aa526e59af0e28bba9
farazzy4u <farazzy4u@gmail.com>
2025-02-15 18:41:33 -0600

    fix: update paths and MIME types for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { resolve } from 'path';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/wake-up-for-suhoor-v1/' : '',
+  base: '/wake-up-for-suhoor-v1/',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
